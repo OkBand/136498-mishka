c19caadb442121442620fec0a98ed0f6f149772a
OkBand <o.bandura42@gmail.com>
2016-12-13 00:53:39 +0300

    Index layout added

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var toggle  = document.querySelector(".main-nav__toggle");
+var close = document.querySelector(".main-nav__close");
+var mainNav = document.querySelector(".main-nav__wrapper");
+
+toggle.addEventListener("click", function(event) {
+	event.preventDefault();
+  toggle.classList.add("visually-hidden");
+  close.classList.remove("visually-hidden");
+  mainNav.classList.remove("main-nav__wrapper--closed");
+});
+
+
+close.addEventListener("click", function(event) {
+	event.preventDefault();
+  close.classList.add("visually-hidden");
+  toggle.classList.remove("visually-hidden");
+  mainNav.classList.add("main-nav__wrapper--closed");
+});
+
+var basket = document.querySelector(".main-nav__basket");
+var modal = document.querySelector(".modal");
+var modalClose = document.querySelector(".modal__close");
+
+basket.addEventListener("click", function(event) {
+  event.preventDefault();
+  modal.classList.remove("visually-hidden");
+});
+
+modalClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  modal.classList.add("visually-hidden");
+});
