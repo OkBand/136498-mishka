678e747e5f1f0ee0ef2fe53bb6f44041cff23545
OkBand <o.bandura42@gmail.com>
2016-12-21 01:56:27 +0300

    added flex layout

--- js/script.js
+++ js/script.js
@@ -1,31 +1,28 @@
-var toggle  = document.querySelector(".main-nav__toggle");
-var close = document.querySelector(".main-nav__close");
-var mainNav = document.querySelector(".main-nav__wrapper");
+var toggle  = document.querySelector('.main-nav__toggle');
+var mainNav = document.querySelector('.main-nav');
 
-toggle.addEventListener("click", function(event) {
-  event.preventDefault();
-  toggle.classList.add("visually-hidden");
-  close.classList.remove("visually-hidden");
-  mainNav.classList.remove("main-nav__wrapper--closed");
+mainNav.classList.remove('main-nav--nojs');
+
+toggle.addEventListener('click', function() {
+  if (mainNav.classList.contains('main-nav--closed')) {
+    mainNav.classList.remove('main-nav--closed');
+    mainNav.classList.add('main-nav--opened');
+  } else {
+    mainNav.classList.add('main-nav--closed');
+    mainNav.classList.remove('main-nav--opened');
+  }
 });
 
-close.addEventListener("click", function(event) {
-event.preventDefault();
-  close.classList.add("visually-hidden");
-  toggle.classList.remove("visually-hidden");
-  mainNav.classList.add("main-nav__wrapper--closed");
+var basket = document.querySelector('.main-nav__basket');
+var modal = document.querySelector('.modal');
+var modalClose = document.querySelector('.modal__close');
+
+basket.addEventListener('click', function(event) {
+  event.preventDefault();
+  modal.classList.remove('visually-hidden');
 });
 
-var basket = document.querySelector(".main-nav__basket");
-var modal = document.querySelector(".modal");
-var modalClose = document.querySelector(".modal__close");
-
-basket.addEventListener("click", function(event) {
+modalClose.addEventListener('click', function(event) {
   event.preventDefault();
-  modal.classList.remove("visually-hidden");
-});
-
-modalClose.addEventListener("click", function(event) {
-  event.preventDefault();
-  modal.classList.add("visually-hidden");
+  modal.classList.add('visually-hidden');
 });
